c2ce454dd3f2de6006694b8325aa8c41d4739c40
biscuit <529324416@qq.com>
2022-02-28 05:11:40 +0800

    Complete Euclid Window

--- euclid.py
+++ euclid.py
@@ -7,7 +7,9 @@
 
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout
 from PyQt5.QtCore import QPoint, QSize
-
+import hashlib
+import json
+import os
 
 def move_window(window: QWidget, evt, pos: QPoint):
     '''drag window by mouse when mouse is moving
@@ -32,11 +34,24 @@ def resize_window(window: QWidget, evt, pos: QPoint):
     window.resize(window.width() + offset.x(), window.height() + offset.y())
     window.parent().update()
 
+def set_name(element: QWidget, name):
+    '''change the object name of target widget
+    and refresh the style'''
+
+    element.setObjectName(name)
+    element.style().unpolish(element)
+    element.style().polish(element)
 
 def label_size(label: QLabel):
     ''' compute the size of label '''
 
     return label.fontMetrics().boundingRect(label.text()).size()
+
+def generate_md5(text:str):
+
+    _5 = hashlib.md5()
+    _5.update(text.encode())
+    return _5.hexdigest()
 
 
 class EuclidNames:
@@ -50,7 +65,6 @@ class EuclidNames:
     SIZEGRIP = "EuclidSizeGrip"
     CONTAINER = "EuclidContainer"
     
-
 
 class EucildUtils:
 
@@ -190,13 +204,17 @@ class _EuclidMoveable(QLabel):
     def __init__(self, parent=None, **kwargs):
         QLabel.__init__(self, parent=parent, **kwargs)
         self._can_move = False
+        self._valid = True
+
+    def set_enabled(self, value):
+        self._valid = value
 
     def mousePressEvent(self, ev):
         self._can_move = True
         self._pos = ev.pos()
 
     def mouseMoveEvent(self, ev):
-        if self._can_move:
+        if self._can_move and self._valid:
             move_window(self, ev, self._pos)
 
     def mouseReleaseEvent(self, ev):
@@ -208,11 +226,15 @@ class _EuclidSizeGrip(_EuclidMoveable):
         super().__init__(parent=parent, **kwargs)
         self.setObjectName(EuclidNames.SIZEGRIP)
         self.setFixedSize(*size)
+        self._valid = True
+
+    def set_enabled(self, value):
+        self._valid = value
 
     def mouseMoveEvent(self, ev):
         '''adjust the size of parent while moving self'''
 
-        if self._can_move:
+        if self._can_move and self._valid:
             resize_window(self.parent(), ev, self._pos)
 
 class _EuclidMiniButton(QPushButton):
@@ -223,7 +245,7 @@ class _EuclidMiniButton(QPushButton):
 class _EuclidTitleBar(QLabel):
     '''Euclid Titlt Bar '''
 
-    def __init__(self, height, parent, title="Euclid Window", padding=5, btnsize=12, **kwargs):
+    def __init__(self, height, parent, lockfunc, title="Euclid Window", padding=5, btnsize=12, **kwargs):
         super().__init__(parent=parent, **kwargs)
         self.setObjectName(EuclidNames.TITLEBAR)
         self.setFixedHeight(height)
@@ -239,6 +261,19 @@ class _EuclidTitleBar(QLabel):
         self._closebtn.setObjectName(EuclidNames.TITLEBAR_CLOSEBTN)
         self._closebtn_padding = padding + btnsize
 
+        self._lockbtn = _EuclidMiniButton(self, size=(btnsize, btnsize))
+        self._lockbtn.setObjectName(EuclidNames.TITLEBAR_LOCKBTN_NORMAL)
+        self._lockbtn.clicked.connect(self._lock)
+        self._lock_window = lockfunc
+
+    def _lock(self):
+        '''lock window and reset style'''
+
+        self.update_lockbtn(self._lock_window())
+        
+    def update_lockbtn(self, value:bool):
+        set_name(self._lockbtn, EuclidNames.TITLEBAR_LOCKBTN_LOCKED if value else EuclidNames.TITLEBAR_LOCKBTN_NORMAL)
+
     def set_title(self, text):
         ''' set window title content, and when content has changed
         the label width would change too'''
@@ -252,19 +287,114 @@ class _EuclidTitleBar(QLabel):
 
     def resizeEvent(self, ev):
         '''adjust position of closebutton and lockbutton '''
+        
         self._closebtn.move(self.width() - self._closebtn_padding, self.padding)
-    
+        self._lockbtn.move(self._closebtn.x() - self._closebtn_padding, self.padding)
+
+def store_window_status(windows:list, filepath:str):
+    '''save all windows' size and positions'''
+
+    o = dict()
+    window_list = dict()
+    for window in windows:
+        cnt = {
+            "pos":[window.x(), window.y()],
+            "size":[window.width(), window.height()],
+            "locked":window.is_locked
+        }
+        window_list.setdefault(window.window_id, cnt)
+    o.setdefault("top", EuclidWindow.top.window_id)
+    o.setdefault("window_list", window_list)
+    with open(filepath, "w", encoding='utf-8') as f:
+        json.dump(o, f)
+
+def restore_window_status(filepath:str):
+    '''restore all windows' size and positions'''
+
+    if os.path.exists(filepath):
+        with open(filepath, "r", encoding='utf-8') as f:
+            o = json.load(f)
+
+        try:
+            window_list = o["window_list"]
+            for kv in window_list.items():
+                win = EuclidWindow.window_list[int(kv[0])]
+                win.move(*kv[1]["pos"])
+                win.resize(*kv[1]["size"])
+                win.set_lock(kv[1]["locked"])
+        except Exception as e:
+            pass
 
 class EuclidWindow(_EuclidMoveable):
+    
+    window_list = list()
+    top = None
 
-    def __init__(self, parent=None, padding=5, sizegrip_size=12, titlebar_height=20, **kwargs):
+    def __init__(self, parent=None, has_title=True, padding=5, sizegrip_size=12, titlebar_height=20, **kwargs):
         super().__init__(parent=parent, **kwargs)
+        self.setObjectName(EuclidNames.WINDOW)
 
-        self._titlebar = _EuclidTitleBar(titlebar_height, self)
+        self.has_title = has_title
+        _titlebar_height = titlebar_height if has_title else 0
+        self._titlebar_height = titlebar_height
+        self._titlebar = _EuclidTitleBar(titlebar_height, self, self._lock)
+        if not has_title:
+            self._titlebar.hide()
         self._sizegrip = _EuclidSizeGrip((sizegrip_size, sizegrip_size), self)
         self._sizegrip_offset = padding + sizegrip_size
-        
+        self._locked = False
+
+        # window id
+        self.window_id = len(EuclidWindow.window_list)
+        EuclidWindow.window_list.append(self)
+
+        if EuclidWindow.top is None:
+            self.raise_()
+            EuclidWindow.top = self
+
+    def enable_title(self, value=True):
+        '''show or hide title according to value '''
+
+        self.has_title = value
+        if value:
+            if self._titlebar.isHidden():
+                self._titlebar.show()
+                self._titlebar.resize(self.width(), self._titlebar_height)
+                self._titlebar.setFixedHeight(self._titlebar_height)
+        else:
+            if not self._titlebar.isHidden():
+                self._titlebar.hide()
+                self._titlebar.setFixedHeight(0)
+
+    def _lock(self):
+        '''lock or unlock window, if window locked, 
+        then it cannot be move or resize '''
+
+        self._sizegrip.set_enabled(self._locked)
+        self.set_enabled(self._locked)
+        self._locked = not self._locked
+
+        # return value to lockbutton
+        return self._locked
+
+    def set_lock(self, value:bool) -> None:
+        ''' lock or unlock window'''
+
+        self._sizegrip.set_enabled(not value)
+        self._titlebar.update_lockbtn(value)
+        self.set_enabled(not value)
+        self._locked = value
+
+    @property
+    def is_locked(self):
+        return self._locked
 
     def resizeEvent(self, a0):
         self._sizegrip.move(self.width() - self._sizegrip_offset, self.height() - self._sizegrip_offset)
-        self._titlebar.resize(self.width(), self._titlebar.height())
+        if self.has_title:
+            self._titlebar.resize(self.width(), self._titlebar.height())
+
+
+
+if __name__ == '__main__':
+    print(generate_md5("hello world"))
--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from euclid import *
 
-class Window(QWidget):
+class Window(QLabel):
 
     def __init__(self):
         super().__init__(None)
         self.setObjectName("EuclidBaseWindow")
-        self.resize(1000, 600)
+        self.resize(int(1308*0.8), int(1120*0.8))
         self.build()
         self.show()
 
@@ -16,8 +16,22 @@ class Window(QWidget):
         with open("./style.qss", encoding='utf-8') as f:
             self.setStyleSheet(f.read())
         self.window = EuclidWindow(parent=self)
-        self.window.setObjectName("EuclidWindow")
         self.window.resize(200, 250)
+
+
+
+        self.window2 = EuclidWindow(parent=self, has_title=False)
+        self.window2.resize(200, 250)
+        btn = EuclidButton()
+        btn.setFixedSize(100, 30)
+        btn.clicked.connect(lambda:self.window2.enable_title())
+        btn.setParent(self.window2)
+
+
+        restore_window_status("./test.json")
+
+    def closeEvent(self, a0):
+        store_window_status(EuclidWindow.window_list, "./test.json")
 
 
 app = QApplication(sys.argv)
